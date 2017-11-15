ac90fff6087b443f7a3dae1c63b277e760d90502
Kevin Peters <kpe@3yourmind.com>
2017-11-15 18:07:11 +0100

    feat(pr-testing-procedure): add module for creating testing procedure
    add markdown generator function which will create the testing procedure out of a two dimensional array of descriotions

--- fire-pr-core/index.js
+++ fire-pr-core/index.js
@@ -1,49 +1,57 @@
 const PrType = require('./pr-type');
 const PrBreaking = require('./pr-breaking');
+const PrTestingProcedure = require('./pr-testing-procedure');
 
-const prTypeConfiguration = [{
-  checked: false,
-  label: 'Bugfix',
-  description: {
-    enabled: false,
-    prefix: null,
-  }
-}, {
-  checked: false,
-  label: 'Feature',
-  description: {
-    enabled: false,
-    prefix: null,
-  }
-}, {
-  checked: false,
-  label: 'Code style update',
-  description: {
-    enabled: false,
-    prefix: null,
-  }
-}, {
-  checked: false,
-  label: 'Refactor',
-  description: {
-    enabled: false,
-    prefix: null,
-  }
-}, {
-  checked: false,
-  label: 'Build-related changes',
-  description: {
-    enabled: false,
-    prefix: null,
-  }
-}, {
-  checked: true,
-  label: 'Other',
-  description: {
-    enabled: true,
-    prefix: ', please describe'
-  }
-}];
+const prTypeConfiguration = [
+  {
+    checked: false,
+    label: 'Bugfix',
+    description: {
+      enabled: false,
+      prefix: null,
+    },
+  },
+  {
+    checked: false,
+    label: 'Feature',
+    description: {
+      enabled: false,
+      prefix: null,
+    },
+  },
+  {
+    checked: false,
+    label: 'Code style update',
+    description: {
+      enabled: false,
+      prefix: null,
+    },
+  },
+  {
+    checked: false,
+    label: 'Refactor',
+    description: {
+      enabled: false,
+      prefix: null,
+    },
+  },
+  {
+    checked: false,
+    label: 'Build-related changes',
+    description: {
+      enabled: false,
+      prefix: null,
+    },
+  },
+  {
+    checked: true,
+    label: 'Other',
+    description: {
+      enabled: true,
+      prefix: ', please describe',
+    },
+  },
+];
 
 const descriptions = {
   Other: 'This is lorem ipsum.',
@@ -60,3 +68,12 @@ const breakingChange = {
 
 const prBreakingMarkdown = PrBreaking.renderMarkdown(breakingChange);
 console.log(prBreakingMarkdown);
+
+const testingProcedure = [
+  ['First step', 'Second step'],
+  ['First step', 'Second step', 'Third step'],
+];
+
+const prTestingProcedureMarkdown = PrTestingProcedure.renderMarkdown(testingProcedure);
+
+console.log(prTestingProcedureMarkdown);
---
+++ fire-pr-core/pr-testing-procedure/index.js
@@ -0,0 +1,27 @@
+const renderMarkdown = (testCases) => {
+  const headline = `## Testing procedure ##
+
+`;
+  if (testCases.length === 0) {
+    return `${headline}*No test cases provided*`;
+  }
+
+  const resultI = testCases.map((testCaseInstructions, index) => {
+    const instructionList = testCaseInstructions.map((testCaseInstruction, index) => {
+      return `${index + 1}. ${testCaseInstruction}`;
+    });
+    const instructionListRepresentation = instructionList.join(`
+`);
+    return `**Case ${index + 1}**
+
+${instructionListRepresentation}`;
+  });
+  const test = resultI.join(`
+
+`);
+  return `${headline}${test}`;
+};
+
+module.exports = {
+  renderMarkdown,
+};
