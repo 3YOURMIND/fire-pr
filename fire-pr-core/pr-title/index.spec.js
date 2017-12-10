82c5a36511299f8f7e2265fe8cdc2d329b85d347
igeligel <kevinigeligeligel@gmail.com>
2017-12-10 20:29:39 +0100

    test(pr-title): add test
    add test for pr title to be formatted right

---
+++ fire-pr-core/pr-title/index.spec.js
@@ -0,0 +1,10 @@
+const PrTitle = require('./index');
+
+describe('Module pr-title', () => {
+  it('Test default payload', () => {
+    const changelogPayload = { heading: 'Fix project behavior', issueNumber: 'MO-3051' };
+    const markdown = PrTitle.renderMarkdown(changelogPayload);
+    const expectedMarkdown = 'Fix project behavior [MO-3051]';
+    expect(markdown).toBe(expectedMarkdown);
+  });
+});
