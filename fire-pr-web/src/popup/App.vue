b3499c13f970f7e573e0354b625b119cc7830e78
James O'Shea <jo@3yourmind.com>
2018-02-06 17:57:56 +0100

    chore(web): integrate prettier

--- fire-pr-web/src/popup/App.vue
+++ fire-pr-web/src/popup/App.vue
@@ -22,40 +22,45 @@ import TheNoBitbucketSite from './components/TheNoBitbucketSite.vue';
 import FirePrCore from 'fire-pr-core';
 
 export default {
-  components: {
-    TheNoBitbucketSite,
-  },
-  data () {
-    return {
-      title: '',
-      bitbucketPullRequestView: false,
-      isLoading: true,
-    };
-  },
-  methods: {
-    updateTitle() {
-      this.$store.dispatch('saveTitle', this.title);
-    },
-  },
-  mounted() {
-    const scriptToExecute = `document.getElementById('id_description').style.height = '500px'`;
-    chrome.tabs.executeScript({
-      code: scriptToExecute,
-    });
+	components: {
+		TheNoBitbucketSite,
+	},
+	data() {
+		return {
+			title: '',
+			bitbucketPullRequestView: false,
+			isLoading: true,
+		};
+	},
+	methods: {
+		updateTitle() {
+			this.$store.dispatch('saveTitle', this.title);
+		},
+	},
+	mounted() {
+		const scriptToExecute = `document.getElementById('id_description').style.height = '500px'`;
+		chrome.tabs.executeScript({
+			code: scriptToExecute,
+		});
 
-    function getSelectedBranch() {
-      const selectBox = document.querySelector('#id_source_group > div.branch-field-container > select');
-      return selectBox.options[selectBox.selectedIndex].text;
-    }
-    chrome.tabs.executeScript({
-      code: '(' + getSelectedBranch + ')();',
-    }, (branchTitle) => {
-      this.isLoading = false;
-      const jiraIssue = branchTitle.toString().match(/B3-([0-9])*/gi)[0];
-      this.$store.dispatch('saveJiraIssue', jiraIssue);
-      this.bitbucketPullRequestView = true;
-    });
-  },
+		function getSelectedBranch() {
+			const selectBox = document.querySelector(
+				'#id_source_group > div.branch-field-container > select',
+			);
+			return selectBox.options[selectBox.selectedIndex].text;
+		}
+		chrome.tabs.executeScript(
+			{
+				code: '(' + getSelectedBranch + ')();',
+			},
+			branchTitle => {
+				this.isLoading = false;
+				const jiraIssue = branchTitle.toString().match(/B3-([0-9])*/gi)[0];
+				this.$store.dispatch('saveJiraIssue', jiraIssue);
+				this.bitbucketPullRequestView = true;
+			},
+		);
+	},
 };
 </script>
 
@@ -65,23 +70,22 @@ export default {
 @import './assets/css/fonts.css';
 
 h4 {
-  font-family: 'Roboto';
+	font-family: 'Roboto';
 }
 
 header {
-  text-align: center;
+	text-align: center;
 }
 
 .fire-app__title-input {
-  margin-top: 10px;
-  width: 80%;
-  margin: auto;
-  display: block;
+	margin-top: 10px;
+	width: 80%;
+	margin: auto;
+	display: block;
 }
 
 .main-container {
-  width: 80%;
-  margin: auto;
+	width: 80%;
+	margin: auto;
 }
-
 </style>
