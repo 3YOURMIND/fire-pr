ffaee4f0848426f61d462feb7ae9473c9dd63299
igeligel <kevinigeligeligel@gmail.com>
2018-02-10 23:16:31 +0100

    feat(web): add view
    add view for pull request title and design it

--- fire-pr-web/src/stories/index.stories.js
+++ fire-pr-web/src/stories/index.stories.js
@@ -4,32 +4,20 @@ import { storiesOf } from '@storybook/vue';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import MyButton from './MyButton.vue';
-import Welcome from './Welcome.vue';
+import PullRequestTitle from '../views/PullRequestTitle.vue';
 
-storiesOf('Welcome', module).add('to Storybook', () => ({
-	components: { Welcome },
-	template: '<welcome :showApp="action" />',
-	methods: { action: linkTo('Button') },
-}));
-
-storiesOf('Button', module)
+storiesOf('PullRequestTitle', module)
+	.add('initial state', () => ({
+		components: { PullRequestTitle },
+		template: `<div style="width: 375px; height: 580px; background-color: #F4F4F9;">
+			<PullRequestTitle />
+		</div>`,
+	}))
 	.add('with text', () => ({
-		components: { MyButton },
-		template: '<my-button @click="action">Hello Button</my-button>',
-		methods: { action: action('clicked') },
-	}))
-	.add('with JSX', () => ({
-		components: { MyButton },
-		render() {
-			return <my-button onClick={this.action}>With JSX</my-button>;
-		},
-		methods: { action: linkTo('clicked') },
-	}))
-	.add('with some emoji', () => ({
-		components: { MyButton },
-		template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
-		methods: { action: action('clicked') },
+		components: { PullRequestTitle },
+		template: `<div style="width: 375px; height: 580px; background-color: #F4F4F9;">
+			<PullRequestTitle :title="'fix(compiler): update weex recycle-list compiler'" />
+		</div>`,
 	}));
 
 /* eslint-enable react/react-in-jsx-scope */
---
+++ fire-pr-web/src/views/PullRequestTitle.vue
@@ -0,0 +1,85 @@
+<template>
+	<div class="pure-g pull-request-title__container" style="padding-top: 20vh;">
+		<div class="pure-u-1" style="text-align: center;">
+			<h1 class="pull-request-title__heading--first">Pull Request Title</h1>
+		</div>
+		<div class="pure-u-1" style="text-align: center;">
+			<input autofocus class="pull-request-title__input" :value="title" />
+		</div>
+		<div class="pure-u-1" style="text-align: center; margin-top: 25vh;">
+			<button class="pull-request-title__button">Next</button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'PullRequestTitle',
+	props: {
+		title: {
+			type: String,
+			default: '',
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+::selection {
+	background: #B8DBD9;
+}
+
+.pull-request-title__heading--first {
+	font-family: 'Roboto';
+	font-weight: 300;
+	color: #2F4550;
+	margin-top: 0.61em;
+	margin-bottom: 0.61em;
+}
+
+.pull-request-title__input {
+	background-color: #F4F4F9;
+	border: 0;
+	border: 1px solid rgba(88, 111, 124, 0.5);
+	color: #586F7C;
+	font-family: 'Roboto';
+	font-weight: 300;
+	border-radius: 5px;
+	font-size: 2em;
+	height: 1.61em;
+	width: 80%;
+	padding-left: 12px;
+	padding-right: 12px;
+}
+
+.pull-request-title__input:focus {
+	outline: none;
+	box-shadow: 0 0 3px #B8DBD9;
+}
+
+.pull-request-title__button {
+	border: 0;
+	background: none;
+	color: #586F7C;
+	font-size: 20px;
+	border: 1px solid rgba(88, 111, 124, 0.5);
+	padding: 0.4em 0.8em;
+	border-radius: 5px;
+
+	&:active {
+		box-shadow: 0 0 3px #B8DBD9;
+		outline: none;
+	}
+
+	&:focus {
+		box-shadow: 0 0 3px #B8DBD9;
+		outline: none;
+	}
+
+	&:target {
+		outline: none;
+		box-shadow: 0 0 3px #B8DBD9;
+	}
+}
+</style>
+
