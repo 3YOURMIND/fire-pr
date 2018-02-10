cdd340c53cd7657e9b0f234fd6e5bd134d03fd97
igeligel <kevinigeligeligel@gmail.com>
2018-02-10 13:29:03 +0100

    refactor(components): no bitbucket site
    refactor the view that you are not on a bitbucket site, remove inline
    styles and use proper scss

--- fire-pr-web/src/components/TheNoBitbucketSite.vue
+++ fire-pr-web/src/components/TheNoBitbucketSite.vue
@@ -1,35 +1,53 @@
 <template>
-	<div style="height: 100vh;">
-		<div
-			class="pure-g"
-			style="text-align: center; position: relative; top: 50%; transform: translateY(-50%);"
-		>
-			<div class="pure-u-1">
-				<TheBitbucketIcon style="width: 30vw" />
-			</div>
-			<div class="pure-u-1" style="margin-top: 2em;">
-				<h2 class="no-pull-request-view">Not in Pull Request View</h2>
-			</div>
-		</div>
-	</div>
+  <div class="no-bitbucket__container">
+    <div class="pure-g no-bitbucket__grid">
+      <div class="pure-u-1">
+        <TheBitbucketIcon class="no-bitbucket__icon" />
+      </div>
+      <div class="pure-u-1 no-bitbucket__heading-container">
+        <h2 class="no-bitbucket__heading--second">Not in Pull Request View</h2>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import TheBitbucketIcon from './TheBitbucketIcon.vue';
 
 export default {
-	name: 'TheNoBitbucketSite',
-	components: {
-		TheBitbucketIcon,
-	},
+  name: 'TheNoBitbucketSite',
+  components: {
+    TheBitbucketIcon,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.no-pull-request-view {
-	font-family: 'Roboto';
-	font-weight: 300;
-	font-size: 2em;
-	letter-spacing: 0;
+.no-bitbucket {
+  &__container {
+    height: 100vh;
+  }
+
+  &__grid {
+    text-align: center;
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  &__icon {
+    width: 30vw;
+  }
+
+  &__heading-container {
+    margin-top: 2em;
+  }
+
+  &__heading--second {
+    font-family: 'Roboto';
+    font-weight: 300;
+    font-size: 2em;
+    letter-spacing: 0;
+  }
 }
 </style>
