08115c000d89239ed7f204064bfe4b6ff83ee7f7
igeligel <kevinigeligeligel@gmail.com>
2018-02-18 22:05:59 +0100

    feat(web): implement buttons
    implement buttons to go back and to the next, also fix styling

--- fire-pr-web/src/views/PullRequestType.vue
+++ fire-pr-web/src/views/PullRequestType.vue
@@ -5,34 +5,52 @@
 				Pull Request Type
 			</h1>
 		</div>
-		<div class="pure-u-1">
-			<FirePrCheckbox :checked.sync="options.bugfix" :label="'Bugfix'" />
+		<div style="padding-left: 6%; padding-right: 6%;">
+			<div class="pure-u-1">
+				<FirePrCheckbox :checked.sync="options.bugfix" :label="'Bugfix'" />
+			</div>
+			<div class="pure-u-1" style="margin-top: 0.61em;">
+				<FirePrCheckbox :checked.sync="options.feature" :label="'Feature'" />
+			</div>
+			<div class="pure-u-1" style="margin-top: 0.61em;">
+				<FirePrCheckbox :checked.sync="options.codeStyle" :label="'Code style'" />
+			</div>
+			<div class="pure-u-1" style="margin-top: 0.61em;">
+				<FirePrCheckbox :checked.sync="options.refactor" :label="'Refactor'" />
+			</div>
+			<div class="pure-u-1" style="margin-top: 0.61em;">
+				<FirePrCheckbox :checked.sync="options.build" :label="'Build-related changes'" />
+			</div>
+			<div class="pure-u-1" style="margin-top: 0.61em;">
+				<FirePrCheckbox :checked.sync="options.other" :label="'Other: Please describe'" />
+				<div class="pull-request-type__input-container" v-if="options.other">
+					<input
+						autofocus
+						type="text"
+						class="pull-request-type__input"
+						v-model="options.otherDescription"
+					/>
+				</div>
+			</div>
 		</div>
-		<div class="pure-u-1">
-			<FirePrCheckbox :checked.sync="options.feature" :label="'Feature'" />
+		<div class="pure-u-1 pull-request-type__button-container">
+			<button
+				class="pull-request-type__button"
+				style="margin-left: 10%; float: left; display: inline;"
+				@click="handleBackButtonClick"
+			>
+				Back
+			</button>
+			<button
+				class="pull-request-type__button"
+				:disabled="buttonEnabled"
+				@click="saveChangeOptions"
+				style="margin-right: 10%; float: right; display: inline;"
+			>
+				Next
+			</button>
+
 		</div>
-		<div class="pure-u-1">
-			<FirePrCheckbox :checked.sync="options.codeStyle" :label="'Code style'" />
-		</div>
-		<div class="pure-u-1">
-			<FirePrCheckbox :checked.sync="options.refactor" :label="'Refactor'" />
-		</div>
-		<div class="pure-u-1">
-			<FirePrCheckbox :checked.sync="options.build" :label="'Build-related changes'" />
-		</div>
-		<div class="pure-u-1">
-			<FirePrCheckbox :checked.sync="options.other" :label="'Other: Please describe'" />
-			<p>
-				<input
-					type="text"
-					v-if="options.other"
-					v-model="options.otherDescription"
-				/>
-			</p>
-		</div>
-		<button :disabled="buttonEnabled" @click="saveChangeOptions">
-			Next
-		</button>
 	</div>
 </template>
 
@@ -79,9 +97,8 @@ export default {
 			this.$store.dispatch('saveChangeOptions', this.options);
 			this.$router.push('/breaking');
 		},
-		test(newValue) {
-			debugger;
-			options.bugfix = newValue;
+		handleBackButtonClick() {
+			this.$router.push('/');
 		},
 	},
 };
@@ -102,5 +119,63 @@ export default {
 
 .pull-request-type__heading-container {
 	text-align: center;
+}
+
+.pull-request-type {
+	&__input {
+		&-container {
+			margin-top: 0.61em;
+		}
+
+		box-sizing: border-box;
+		background-color: #f4f4f9;
+		border: 0;
+		border: 1px solid rgba(88, 111, 124, 0.5);
+		color: #586f7c;
+		font-family: 'Roboto';
+		font-weight: 300;
+		border-radius: 3px;
+		font-size: 1em;
+		width: 100%;
+		padding-left: 6px;
+		padding-right: 6px;
+		padding-top: calc(0.61em / 2);
+		padding-bottom: calc(0.61em / 2);
+
+		&:focus {
+			outline: none;
+			box-shadow: 0 0 3px #b8dbd9;
+		}
+	}
+
+	&__button {
+		&-container {
+			position: absolute;
+			bottom: 6%;
+		}
+
+		border: 0;
+		background: none;
+		color: #586f7c;
+		font-size: 20px;
+		border: 1px solid rgba(88, 111, 124, 0.5);
+		padding: 0.4em 0.8em;
+		border-radius: 5px;
+
+		&:active {
+			box-shadow: 0 0 3px #b8dbd9;
+			outline: none;
+		}
+
+		&:focus {
+			box-shadow: 0 0 3px #b8dbd9;
+			outline: none;
+		}
+
+		&:target {
+			outline: none;
+			box-shadow: 0 0 3px #b8dbd9;
+		}
+	}
 }
 </style>
