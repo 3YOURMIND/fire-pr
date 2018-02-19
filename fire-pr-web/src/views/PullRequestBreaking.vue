d0a32d5764eeb1687f5a503af3a1a0de7333f98b
igeligel <kevinigeligeligel@gmail.com>
2018-02-19 17:24:53 +0100

    refactor(web): pull request breaking
    add better radio checkboxes

--- fire-pr-web/src/views/PullRequestBreaking.vue
+++ fire-pr-web/src/views/PullRequestBreaking.vue
@@ -1,17 +1,26 @@
 <template>
 	<div class="pure-g pull-request-breaking__container">
-		<div class="pure-u-1">
-			<h3>Does this PR introduce a breaking change?</h3>
+		<div class="pure-u-1 pull-request-breaking__heading-container">
+			<h1 class="pull-request-breaking__heading--first">
+				Breaking changes
+			</h1>
 		</div>
-		<div class="pure-u-1">
-			<input type="radio" name="breaking" @click="updateBreaking(true)" />
-			<label>Yes</label>
-			<input v-if="breaking" type="text" v-model="breakingChangeText" />
-		</div>
-		<div class="pure-u-1">
-			<p>
-				<input type="radio" name="breaking" @click="updateBreaking(false)" />No
-			</p>
+		<div style="padding-left: 6%; padding-right: 6%;">
+			<div class="pure-u-1">
+				<label class="control control--radio" @click="updateBreaking(true)">
+					<span>Yes</span>
+					<input type="radio" name="radio" :checked="breaking"/>
+					<div class="control__indicator"></div>
+				</label>
+				<input v-if="breaking" type="text" v-model="breakingChangeText" />
+			</div>
+			<div class="pure-u-1">
+				<label class="control control--radio" @click="updateBreaking(false)">
+					<span>No</span>
+					<input type="radio" name="radio" :checked="!breaking"/>
+					<div class="control__indicator"></div>
+				</label>
+			</div>
 		</div>
 		<div class="pure-u-1 pull-request-breaking__button-container">
 			<FirePrButton
@@ -41,7 +50,7 @@ export default {
 	},
 	data() {
 		return {
-			breaking: null,
+			breaking: false,
 			breakingChangeText: '',
 		};
 	},
@@ -78,6 +87,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.pull-request-breaking__heading--first {
+	font-family: 'Roboto';
+	font-weight: 300;
+	color: #2f4550;
+	margin-top: 0.61em;
+	margin-bottom: 0.61em;
+}
+
+.pull-request-breaking__heading-container {
+	text-align: center;
+}
+
 .pull-request-breaking__container {
 	font-size: 16px;
 }
@@ -85,5 +106,58 @@ export default {
 .pull-request-breaking__button-container {
 	position: absolute;
 	bottom: 6%;
+}
+
+.control {
+	display: block;
+	position: relative;
+	padding-left: 30px;
+	margin-bottom: 15px;
+	cursor: pointer;
+	font-size: 18px;
+}
+.control input {
+	position: absolute;
+	z-index: -1;
+	opacity: 0;
+}
+.control__indicator {
+	position: absolute;
+	top: 2px;
+	left: 0;
+	height: 20px;
+	width: 20px;
+	background: rgb(244, 244, 249);
+	border: 1px solid rgba(47, 69, 80, 0.1);
+}
+.control--radio .control__indicator {
+	border-radius: 50%;
+}
+.control:hover input ~ .control__indicator,
+.control input:focus ~ .control__indicator {
+	background: rgb(244, 244, 249);
+}
+.control input:checked ~ .control__indicator {
+	background: rgba(47, 69, 80, 0.5);
+}
+.control__indicator:after {
+	content: '';
+	position: absolute;
+	display: none;
+}
+.control input:checked ~ .control__indicator:after {
+	display: block;
+}
+.control--radio .control__indicator:after {
+	$size: 16px;
+	left: calc(50% - (#{$size} / 2));
+	top: calc(50% - (#{$size} / 2));
+	height: $size;
+	width: $size;
+	border-radius: 50%;
+	background-color: #f4f4f9;
+}
+.control--radio input:disabled ~ .control__indicator:after {
+	background: #7b7b7b;
 }
 </style>
