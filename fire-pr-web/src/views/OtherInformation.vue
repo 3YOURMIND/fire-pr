f6f0f8f761ff387666ed44b73464b19923b8b078
igeligel <kevinigeligeligel@gmail.com>
2018-02-26 00:30:51 +0100

    refactor(views): other information
    refactor style of other information to fit fire pr style

--- fire-pr-web/src/views/OtherInformation.vue
+++ fire-pr-web/src/views/OtherInformation.vue
@@ -1,7 +1,12 @@
 <template>
 	<div class="pure-g other-information__container">
-		<div class="pure-u-1">
-			<ol v-if="informations.length > 0">
+		<div class="pure-u-1 pull-request-other-information__heading-container">
+			<h1 class="pull-request-other-information__heading--first">
+				Other Information
+			</h1>
+		</div>
+		<div class="pure-u-1" style="margin-bottom: 0.61em; padding-left: 6%; padding-right: 6%; font-family: 'Roboto'; font-weight: 300; color: #2f4550;">
+			<ol v-if="informations.length > 0" style="padding-left: 6%; margin: 0;">
 				<li
 					v-for="information in informations"
 					:key="information"
@@ -9,20 +14,32 @@
 				/>
 			</ol>
 		</div>
-		<div class="pure-u-1">
+		<div class="pure-u-1" style="margin-bottom: 0.61em; padding-left: 6%; padding-right: 6%; font-family: 'Roboto'; font-weight: 300; color: #2f4550;">
 			<div v-if="informations.length < 1">No Information given</div>
 		</div>
 		<div class="pure-u-1">
 			<hr />
 		</div>
-		<div class="pure-u-1">
+		<div class="pure-u-1" style="margin-top: 0.61em; padding-left: 6%; padding-right: 6%; font-family: 'Roboto'; font-weight: 300; color: #2f4550;">
 			<label>Other Information Note</label>
 		</div>
-		<div class="pure-u-1">
-			<input type="text" name="merger" v-model="text" />
+		<div class="pure-u-1" style="margin-top: 0.61em; padding-left: 6%; padding-right: 6%; font-family: 'Roboto'; font-weight: 300; color: #2f4550;">
+			<div class="pull-request-other-information__input-container">
+				<input
+					autofocus
+					type="text"
+					name="merger"
+					class="pull-request-other-information__input"
+					v-model="text"
+				/>
+			</div>
 		</div>
-		<div class="pure-u-1">
-			<button @click="addOtherInformation">Add other information</button>
+		<div class="pure-u-1" style="margin-top: 0.61em; padding-left: 6%; padding-right: 6%; font-family: 'Roboto'; font-weight: 300; color: #2f4550;">
+			<FirePrButton
+				:label="'Add other information'"
+				@click="addOtherInformation"
+				style="width: 100%;"
+			/>
 		</div>
 		<div class="pure-u-1 pull-request-other-information__button-container">
 			<FirePrButton
@@ -70,6 +87,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.pull-request-other-information__heading-container {
+	text-align: center;
+}
+
+.pull-request-other-information__heading--first {
+	font-family: 'Roboto';
+	font-weight: 300;
+	color: #2f4550;
+	margin-top: 0.61em;
+	margin-bottom: 0.61em;
+}
+
 .pull-request-other-information__button-container {
 	position: absolute;
 	bottom: 6%;
@@ -77,5 +106,27 @@ export default {
 
 .other-information__container {
 	font-size: 16px;
+}
+
+.pull-request-other-information__input {
+	box-sizing: border-box;
+	background-color: #f4f4f9;
+	border: 0;
+	border: 1px solid rgba(88, 111, 124, 0.5);
+	color: #586f7c;
+	font-family: 'Roboto';
+	font-weight: 300;
+	border-radius: 3px;
+	font-size: 1em;
+	width: 100%;
+	padding-left: 6px;
+	padding-right: 6px;
+	padding-top: calc(0.61em / 2);
+	padding-bottom: calc(0.61em / 2);
+
+	&:focus {
+		outline: none;
+		box-shadow: 0 0 3px #b8dbd9;
+	}
 }
 </style>
