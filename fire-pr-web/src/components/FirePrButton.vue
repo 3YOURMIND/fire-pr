7bac1701ec39691f2aa1e6e8f0d5107a6231c5bf
igeligel <kevinigeligeligel@gmail.com>
2018-03-18 19:48:20 +0100

    refactor(components): fire pr button
    make event propagating possible and refactor scss to mixins to reduce
    scss

--- fire-pr-web/src/components/FirePrButton.vue
+++ fire-pr-web/src/components/FirePrButton.vue
@@ -18,25 +18,23 @@ export default {
 		},
 	},
 	methods: {
-		onButtonClick() {
-			this.$emit('click');
+		onButtonClick($event) {
+			this.$emit('click', $event);
 		},
 	},
 };
 </script>
 
 <style lang="scss" scoped>
-.fire-pr__button {
-	border: 0;
+@mixin common-button-style() {
 	background: none;
-	color: #586f7c;
-	font-size: 20px;
-	border: 1px solid rgba(88, 111, 124, 0.5);
-	padding: 0.4em 0.8em;
+	border: 0;
 	border-radius: 5px;
-	transition: all 0.2s ease;
 	font-family: 'Roboto';
+	font-size: 20px;
 	font-weight: 300;
+	padding: 0.4em 0.8em;
+	transition: all 0.2s ease;
 
 	&:active {
 		box-shadow: 0 0 3px #b8dbd9;
@@ -54,32 +52,18 @@ export default {
 	}
 }
 
+.fire-pr__button {
+	@include common-button-style();
+
+	border: 1px solid rgba(88, 111, 124, 0.5);
+	color: #586f7c;
+}
+
 .fire-pr__button--disabled {
-	border: 0;
-	background: none;
-	color: rgba(88, 111, 124, 0.2);
-	font-size: 20px;
+	@include common-button-style();
+
 	border: 1px solid rgba(88, 111, 124, 0.2);
-	padding: 0.4em 0.8em;
-	border-radius: 5px;
+	color: rgba(88, 111, 124, 0.2);
 	cursor: not-allowed;
-	transition: all 0.2s ease;
-	font-family: 'Roboto';
-	font-weight: 300;
-
-	&:active {
-		box-shadow: 0 0 3px #b8dbd9;
-		outline: none;
-	}
-
-	&:focus {
-		box-shadow: 0 0 3px #b8dbd9;
-		outline: none;
-	}
-
-	&:target {
-		outline: none;
-		box-shadow: 0 0 3px #b8dbd9;
-	}
 }
 </style>
