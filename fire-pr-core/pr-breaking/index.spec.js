919080b29a0ee6de565518fc381f67fd5185c3d3
igeligel <kevinigeligeligel@gmail.com>
2018-03-20 22:37:43 +0100

    test(core): breaking and changelog
    fix tests for breaking and changelog markdown creation

--- fire-pr-core/pr-breaking/index.spec.js
+++ fire-pr-core/pr-breaking/index.spec.js
@@ -1,52 +1,52 @@
 import PrBreakingModule from './index';
 
 describe('Test pr-breaking', () => {
-  it('Breaking change with description', () => {
-    const payload = {
-      exists: true,
-      description:
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
-    };
-    const expectedResult = `## Does this PR introduce a breaking change? ##
+	it('Breaking change with description', () => {
+		const payload = {
+			exists: true,
+			description:
+				'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+		};
+		const expectedResult = `## Does this PR introduce a breaking change? ##
 
-[✖] No
-[✔] Yes: Lorem Ipsum is simply dummy text of the printing and typesetting industry`;
-    const result = PrBreakingModule.renderMarkdown(payload);
-    expect(result).toBe(expectedResult);
-  });
+* [✖] No
+* [✔] Yes: Lorem Ipsum is simply dummy text of the printing and typesetting industry`;
+		const result = PrBreakingModule.renderMarkdown(payload);
+		expect(result).toBe(expectedResult);
+	});
 
-  it('Breaking change without description', () => {
-    const payload = {
-      exists: true,
-      description: null,
-    };
-    expect(() => {
-      PrBreakingModule.renderMarkdown(payload);
-    }).toThrow();
-  });
+	it('Breaking change without description', () => {
+		const payload = {
+			exists: true,
+			description: null,
+		};
+		expect(() => {
+			PrBreakingModule.renderMarkdown(payload);
+		}).toThrow();
+	});
 
-  it('Non-breaking change without description', () => {
-    const payload = {
-      exists: false,
-      description: null,
-    };
-    const expectedResult = `## Does this PR introduce a breaking change? ##
+	it('Non-breaking change without description', () => {
+		const payload = {
+			exists: false,
+			description: null,
+		};
+		const expectedResult = `## Does this PR introduce a breaking change? ##
 
-[✔] No
-[✖] Yes`;
-    expect(PrBreakingModule.renderMarkdown(payload)).toBe(expectedResult);
-  });
+* [✔] No
+* [✖] Yes`;
+		expect(PrBreakingModule.renderMarkdown(payload)).toBe(expectedResult);
+	});
 
-  it('Non-breaking change with description', () => {
-    const payload = {
-      exists: false,
-      description:
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
-    };
-    const expectedResult = `## Does this PR introduce a breaking change? ##
+	it('Non-breaking change with description', () => {
+		const payload = {
+			exists: false,
+			description:
+				'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+		};
+		const expectedResult = `## Does this PR introduce a breaking change? ##
 
-[✔] No
-[✖] Yes`;
-    expect(PrBreakingModule.renderMarkdown(payload)).toBe(expectedResult);
-  });
+* [✔] No
+* [✖] Yes`;
+		expect(PrBreakingModule.renderMarkdown(payload)).toBe(expectedResult);
+	});
 });
--- fire-pr-core/pr-changelog/index.spec.js
+++ fire-pr-core/pr-changelog/index.spec.js
@@ -1,30 +1,30 @@
 import PrChangelogModule from './index';
 
 describe('Test pr-changelog', () => {
-  it('Change default payload', () => {
-    const changelogPayload = {
-      note: {
-        description: 'Lorem ipsum',
-        issueNumber: 3059,
-        baseUrl: 'https://testOrganization.atlassian.net',
-        projectAbbreviation: 'B3',
-      },
-      pictures: [
-        'http://via.placeholder.com/325x150',
-        'http://via.placeholder.com/325x150',
-      ],
-    };
-    const markdown = PrChangelogModule.renderMarkdown(changelogPayload);
-    const expectedMarkdown = `## Changelog ##
+	it('Change default payload', () => {
+		const changelogPayload = {
+			note: {
+				description: 'Lorem ipsum',
+				issueNumber: 3059,
+				baseUrl: 'https://testOrganization.atlassian.net',
+				projectAbbreviation: 'B3',
+			},
+			pictures: [
+				'http://via.placeholder.com/325x150',
+				'http://via.placeholder.com/325x150',
+			],
+		};
+		const markdown = PrChangelogModule.renderMarkdown(changelogPayload);
+		const expectedMarkdown = `## Changelog ##
 
-### Note ###
+**Note**
 
 - Lorem ipsum, closes [#3059](https://testOrganization.atlassian.net/browse/B3-3059)
 
-### Pictures ###
+**Pictures**
 
 - ![](http://via.placeholder.com/325x150)
 - ![](http://via.placeholder.com/325x150)`;
-    expect(markdown).toBe(expectedMarkdown);
-  });
+		expect(markdown).toBe(expectedMarkdown);
+	});
 });
