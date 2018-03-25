c35b74c18fc390644fbaeffd4a6d2d38602f2a0a
igeligel <kevinigeligeligel@gmail.com>
2018-03-25 21:03:36 +0200

    feat(site): clean up
    clean up site

--- fire-pr-site/pages/index.vue
+++ fire-pr-site/pages/index.vue
@@ -1,18 +1,6 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        fire-pr-test
-      </h1>
-      <h2 class="subtitle">
-        My ultimate Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <h1>Fire PR</h1>
   </section>
 </template>
 
