2adb7f64dba1101b4bf2c1d6d037e7701d533252
igeligel <kevinigeligeligel@gmail.com>
2018-03-25 23:41:09 +0200

    feat(site): design main page
    design index page

--- fire-pr-site/layouts/default.vue
+++ fire-pr-site/layouts/default.vue
@@ -1,14 +1,117 @@
 <template>
   <div>
+    <div class="navigation">
+      <router-link class="navigation__logo-container" :to="'/'">
+        <img class="navigation__logo" src="../assets/logo.svg"/>
+        <div class="navigation__title">PR</div>
+      </router-link>
+      <div class="navigation__menu" style="display: inline-block;">
+        <div class="navigation__menu-item-container">
+          <router-link :to="'/support'">Support</router-link>
+        </div>
+        <div class="navigation__menu-item-container">
+          <router-link :to="'/why-fire-pr'">Why fire-pr?</router-link>
+        </div>
+        <div class="navigation__menu-item-container">
+          <router-link :to="'/download'">Download</router-link>
+        </div>
+        <div class="navigation__menu-item-container">
+          <router-link :to="'/about'">About</router-link>
+        </div>
+      </div>
+    </div>
     <nuxt/>
   </div>
 </template>
 
+<script>
+export default {
+	name: 'DefaultLayout',
+};
+</script>
+
+
+<style lang="scss" scoped>
+.navigation__menu-item-container:not(:first-child) {
+	margin-right: 32px;
+}
+
+.navigation__menu-item-container {
+	font-size: 19px;
+	font-weight: 700;
+	line-height: 50px;
+	margin-top: 10px;
+	float: right;
+	display: inline-block;
+
+	& > a {
+		color: #586f7c;
+	}
+}
+
+.navigation__menu-item-container > a:link {
+	text-decoration: none;
+}
+
+.navigation__menu-item-container > a:visited {
+	text-decoration: none;
+}
+
+.navigation__menu-item-container > a:hover {
+	text-decoration: none;
+	color: #2f4550;
+}
+
+.navigation__menu-item-container > a:active {
+	text-decoration: none;
+}
+
+.navigation__title {
+	font-family: 'Roboto';
+	display: inline-block;
+	line-height: 50px;
+	float: left;
+	margin-top: 10px;
+	font-size: 32px;
+	font-weight: 700;
+	color: #2f4550;
+	margin-left: 10px;
+}
+
+.navigation__logo {
+	height: 100%;
+	display: inline-block;
+	float: left;
+}
+
+.navigation__menu {
+	padding-top: 20px;
+	padding-bottom: 20px;
+	height: 100%;
+	display: inline-block;
+	float: right;
+}
+
+.navigation__logo-container {
+	padding-top: 20px;
+	padding-bottom: 20px;
+	height: 100%;
+	display: inline-block;
+}
+
+.navigation {
+	width: 100%;
+	height: 100px;
+	padding-left: calc((100% - 1000px) / 2);
+	padding-right: calc((100% - 1000px) / 2);
+}
+</style>
+
+
 <style>
 html {
-	/* font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-		Roboto, 'Helvetica Neue', Arial, sans-serif;
-	font-size: 16px;
+	font-family: 'Roboto';
+	/* font-size: 16px;
 	word-spacing: 1px; */
 	-ms-text-size-adjust: 100%;
 	-webkit-text-size-adjust: 100%;
--- fire-pr-site/pages/index.vue
+++ fire-pr-site/pages/index.vue
@@ -1,6 +1,22 @@
 <template>
   <section class="container">
-    <h1>Fire PR</h1>
+    <div class="pure-g" style="width: 100%;">
+      <div class="pure-u-1-2 pure-g">
+        <div class="content">
+          <h1 class="pure-u-1 content__heading--first">Crafting Pull Requests</h1>
+          <span class="pure-u-1 content__description">Optimize the developer experience by generating pull requests more quickly and more reliant.</span>
+          <div class="pure-u-1" style="margin-top: 24px;">
+            <a :href="downloadUrl" rel="noopener noreferrer" target="_blank" class="content__button" v-text="downloadFor"></a>
+          </div>
+          <span class="pure-u-1 content__button-description">Work with JIRA + Bitbucket</span>
+        </div>
+      </div>
+      <div class="pure-u-1-2">
+        <div class="image__container">
+          <img class="content-image" src="../assets/extension-example.svg" />
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -11,34 +27,146 @@ export default {
 	components: {
 		Logo,
 	},
+	data() {
+		return {
+			browser: '',
+		};
+	},
+	computed: {
+		downloadFor() {
+			return `Download for ${this.browser}`;
+		},
+		downloadUrl() {
+			if (this.browser === 'Chrome') {
+				return 'https://chrome.google.com/webstore/detail/fire-pr-chrome-extension/mcpcdbmbifcjeeedncbcajkljmdjdgoh';
+			}
+			if (this.browser === 'Firefox') {
+				return 'https://addons.mozilla.org/en-US/firefox/addon/fire-pr/';
+			}
+			return 'https://chrome.google.com/webstore/detail/fire-pr-chrome-extension/mcpcdbmbifcjeeedncbcajkljmdjdgoh';
+		},
+	},
+	created() {
+		this.browser = this.getBrowser();
+	},
+	methods: {
+		getBrowser() {
+			if (process.browser) {
+				var isOpera =
+					(!!window.opr && !!opr.addons) ||
+					!!window.opera ||
+					navigator.userAgent.indexOf(' OPR/') >= 0;
+				var isFirefox = typeof InstallTrigger !== 'undefined';
+				if (isFirefox) {
+					return 'Firefox';
+				}
+				var isSafari =
+					/constructor/i.test(window.HTMLElement) ||
+					(function(p) {
+						return p.toString() === '[object SafariRemoteNotification]';
+					})(
+						!window['safari'] ||
+							(typeof safari !== 'undefined' && safari.pushNotification),
+					);
+				var isIE = /*@cc_on!@*/ false || !!document.documentMode;
+				var isEdge = !isIE && !!window.StyleMedia;
+				var isChrome = !!window.chrome && !!window.chrome.webstore;
+				var isBlink = (isChrome || isOpera) && !!window.CSS;
+				if (isChrome) {
+					return 'Chrome';
+				}
+			}
+			return 'Chrome';
+		},
+	},
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.image__container {
+	margin-left: 15%;
+	height: 60vh;
+	margin-top: 10vh;
+}
+
+.content-image {
+	transform: rotate(12deg);
+	height: 100%;
+	width: auto;
+}
+
+.content {
+	margin-top: 18vh;
+}
+
+.content__button-description {
+	color: #b8dbd9;
+	font-weight: 500;
+	margin-left: 14px;
+	font-size: 16px;
+	margin-top: 0.6em;
+	font-family: 'Roboto';
+}
+
+.content__button {
+	display: table-cell;
+	vertical-align: middle;
+	margin-top: 24px;
+	background: white;
+	border: 2px solid rgba(88, 111, 124, 0.5);
+	border-radius: 10px;
+	font-size: 19px;
+	padding-top: 11px; // (19 * 1.6) / 2
+	padding-bottom: 11px;
+	padding-left: 40px;
+	padding-right: 40px;
+	color: #586f7c;
+	font-weight: 700;
+	margin-left: 10px;
+	box-sizing: border-box;
+	font-family: 'Roboto';
+}
+
+.content__button:link {
+	text-decoration: none;
+}
+
+.content__button:visited {
+	text-decoration: none;
+}
+
+.content__button:hover {
+	text-decoration: none;
+}
+
+.content__button:active {
+	text-decoration: none;
+}
+
+.content__heading--first {
+	font-size: 38px;
+	text-align: left;
+	font-weight: 700;
+	margin-bottom: 0;
+	margin-top: 0;
+	max-width: 90%;
+	font-family: 'Roboto';
+}
+
+.content__description {
+	margin-top: 14px;
+	font-size: 24px;
+	text-align: left;
+	font-weight: 700;
+	color: #586f7c;
+	max-width: 90%;
+	font-family: 'Roboto';
+}
+
 .container {
-	min-height: 100vh;
 	display: flex;
-	justify-content: center;
-	align-items: center;
-	text-align: center;
-}
-.title {
-	font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-		'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif; /* 1 */
-	display: block;
-	font-weight: 300;
-	font-size: 100px;
-	color: #35495e;
-	letter-spacing: 1px;
-}
-.subtitle {
-	font-weight: 300;
-	font-size: 42px;
-	color: #526488;
-	word-spacing: 5px;
-	padding-bottom: 15px;
-}
-.links {
-	padding-top: 15px;
+	padding-left: calc((100% - 1000px) / 2);
+	padding-right: calc((100% - 1000px) / 2);
+	font-family: 'Roboto';
 }
 </style>
