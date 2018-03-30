166a567b7f3ff8be50b6b206ef0c42843c9c4752
igeligel <kevinigeligeligel@gmail.com>
2018-03-30 23:42:22 +0200

    feat(site): add imprint

---
+++ fire-pr-site/pages/imprint.vue
@@ -0,0 +1,30 @@
+<template>
+  <div  class="container">
+    <div class="pure-g" style="width: 100%">
+      <div class="pure-u-1" style="font-family: 'Roboto'; display: flex; justify-content: center; align-content: center;">
+        <div>
+          <h1 style="font-weight: 500; font-size: 38px; margin: 0;">Imprint</h1>
+          <p style="margin-top: 0.6em; line-height: 1.6em;">Kevin Peters C/O 3YOURMIND GmbH</p>
+          <p style="line-height: 1.6em;">Bismarckstraße 10-12</p>
+          <p style="line-height: 1.6em;">10625 Berlin</p>
+          <p style="line-height: 1.6em;">+49 30 555 78748</p>
+          <p style="line-height: 1.6em;">kpe@3yourmind.com</p>
+
+          <h2 style="font-weight: 500; font-size: 23px; margin-top: 0.6em;">Disclaimer - rechtliche Hinweise</h2>
+          <p style="font-weight: 500; margin-top: 0.6em;">Auskunfts- und Widerrufsrecht</p>
+          <p style="margin-top: 0.6em; line-height: 1.6em;">Sie haben jederzeit das Recht, sich unentgeltlich und unverzüglich über die zu Ihrer Person erhobenen Daten zu erkundigen. Ebenfalls können Sie Ihre Zustimmung zur Verwendung Ihrer angegebenen persönlichen Daten mit Wirkung für die Zukunft widerrufen. Hierfür wenden Sie sich bitte an den im Impressum angegebenen Diensteanbieter.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'Imprint',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/layout.scss';
+</style>
