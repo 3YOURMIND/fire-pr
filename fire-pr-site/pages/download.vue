9f0cf5a38a82312acb95c244185b8dbd38c8009b
igeligel <kevinigeligeligel@gmail.com>
2018-03-31 18:10:19 +0200

    feat(site): add download site
    add download site which works across paltforms

--- fire-pr-site/pages/download.vue
+++ fire-pr-site/pages/download.vue
@@ -1,6 +1,21 @@
 <template>
-  <section class="container">
-    <h1>Download</h1>
+  <section class="container" style="display: flex; justify-content: center; align-items: center;">
+    <div class="pure-g" style="width: 100%;">
+      <div class="pure-u-sm-1-2 pure-u-1">
+        <div style="display: flex; justify-content: center; align-items: center; height: 100%;">
+          <a rel="noopener noreferrer" target="_blank" href="https://chrome.google.com/webstore/detail/fire-pr-chrome-extension/mcpcdbmbifcjeeedncbcajkljmdjdgoh" style="width: 100%;">
+            <img class="store__image" src="../assets/chrome-webstore.svg" alt="" />
+          </a>
+        </div>
+      </div>
+      <div class="pure-u-sm-1-2 pure-u-1">
+        <div style="display: flex; justify-content: center; align-items: center; height: 100%;">
+          <a rel="noopener noreferrer" target="_blank" href="https://addons.mozilla.org/en-US/firefox/addon/fire-pr/" style="width: 100%;">
+            <img class="store__image" style="width: 100%;" src="../assets/firefox-addon.svg" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -12,4 +27,17 @@ export default {
 
 <style lang="scss" scoped>
 @import '../assets/scss/layout.scss';
+
+.store__image {
+	width: 100%;
+	filter: grayscale(1);
+	transform: scale(0.85);
+	transition: all 0.15s ease-in;
+
+	&:hover {
+		filter: grayscale(0);
+		transition: all 0.15s ease-out;
+		transform: scale(0.9);
+	}
+}
 </style>
