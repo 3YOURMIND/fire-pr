c7a377d362d036f5a04df4b7f09c95fb7b06b989
igeligel <kevinigeligeligel@gmail.com>
2018-04-02 20:36:57 +0200

    refactor(site): rename classes

--- fire-pr-site/components/TechnologyList.vue
+++ fire-pr-site/components/TechnologyList.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="technology-logo__container">
-    <div v-for="(technology, index) in technologies" :key="index" class="technology-logo__item-container">
+  <div class="technology-list__container">
+    <div v-for="(technology, index) in technologies" :key="index" class="technology-list__item-container">
       <a :href="technology.website" target="_blank" rel="noopener noreferrer">
         <img :src="technology.logo" :alt="technology.logoDescription" />
       </a>
@@ -18,7 +18,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.technology-logo__container {
+.technology-list__container {
 	display: flex;
 	justify-content: center;
 	max-width: 1000px;
@@ -27,7 +27,7 @@ export default {
 	flex-wrap: wrap;
 }
 
-.technology-logo__item-container {
+.technology-list__item-container {
 	min-width: 168px;
 	max-width: 168px;
 	margin-top: 20px;
