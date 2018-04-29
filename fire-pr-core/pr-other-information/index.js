07505a12925b2e03664416462cb2daa5bd0edb16
igeligel <kevinigeligeligel@gmail.com>
2018-04-29 14:41:34 +0200

    fix(core): other information
    fix formatting of backticks in other information module

--- fire-pr-core/pr-other-information/index.js
+++ fire-pr-core/pr-other-information/index.js
@@ -1,18 +1,18 @@
-const renderMarkdown = (otherInformation) => {
-  const headline = `## Other information ##
+const renderMarkdown = otherInformation => {
+	const headline = `## Other information ##
 
 `;
-  if (otherInformation.length === 0) {
-    return `${headline}*No other information provided*`;
-  }
-  const otherInformationListRepresentation = otherInformation.map(
-    (otherInformationElement) => {
-      return `- ${otherInformationElement}`;
-    },
-  );
-  return `${headline}${otherInformationListRepresentation.join('\n')}`;
+	if (otherInformation.length === 0) {
+		return `${headline}*No other information provided*`;
+	}
+	const otherInformationListRepresentation = otherInformation.map(
+		otherInformationElement => {
+			return `- ${otherInformationElement.replace('`', '`')}`;
+		},
+	);
+	return `${headline}${otherInformationListRepresentation.join('\n')}`;
 };
 
 export default {
-  renderMarkdown,
+	renderMarkdown,
 };
--- fire-pr-core/pr-other-information/index.spec.js
+++ fire-pr-core/pr-other-information/index.spec.js
@@ -1,22 +1,42 @@
 import PrOtherInformation from './index';
 
 describe('Module pr-other-information', () => {
-  it('Test default payload', () => {
-    const changelogPayload = ['first line', 'second line'];
-    const markdown = PrOtherInformation.renderMarkdown(changelogPayload);
-    const expectedMarkdown = `## Other information ##
+	it('Test default payload', () => {
+		const changelogPayload = ['first line', 'second line'];
+		const markdown = PrOtherInformation.renderMarkdown(changelogPayload);
+		const expectedMarkdown = `## Other information ##
 
 - first line
 - second line`;
-    expect(markdown).toBe(expectedMarkdown);
-  });
+		expect(markdown).toBe(expectedMarkdown);
+	});
 
-  it('Test empty payload', () => {
-    const changelogPayload = [];
-    const markdown = PrOtherInformation.renderMarkdown(changelogPayload);
-    const expectedMarkdown = `## Other information ##
+	it('Test empty payload', () => {
+		const changelogPayload = [];
+		const markdown = PrOtherInformation.renderMarkdown(changelogPayload);
+		const expectedMarkdown = `## Other information ##
 
 *No other information provided*`;
-    expect(markdown).toBe(expectedMarkdown);
-  });
+		expect(markdown).toBe(expectedMarkdown);
+	});
+
+	it('Test other information with backtick', () => {
+		const changelogPayload = ['first line', 'second line'];
+		const markdown = PrOtherInformation.renderMarkdown(changelogPayload);
+		const expectedMarkdown = `## Other information ##
+
+- first line
+- second line`;
+		expect(markdown).toBe(expectedMarkdown);
+	});
+
+	it("should render backtick's properly", () => {
+		const changelogPayload = ['this should look like `test`.', 'second line'];
+		const markdown = PrOtherInformation.renderMarkdown(changelogPayload);
+		const expectedMarkdown = `## Other information ##
+
+- this should look like \`test\`.
+- second line`;
+		expect(markdown).toBe(expectedMarkdown);
+	});
 });
